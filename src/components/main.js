5d3b275701cd36e7275ad78790eedd379a964e58
andradejs <caped.2022123tads0039@aluno.ifpi.edu.br>
2024-06-07 11:50:18 -0300

    lista de tarefa com react

---
+++ src/components/main.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import { FaPlus } from "react-icons/fa";
+import { FaEdit, FaWindowClose } from "react-icons/fa";
+import "./main.css";
+
+export default class Main extends Component {
+  state = {
+    newTask: "",
+    tasks: [],
+    index: -1,
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      newTask: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tasks } = this.state;
+    let { newTask } = this.state;
+    newTask = newTask.trim();
+
+    if (tasks.indexOf(newTask) !== -1)
+      return alert(`A tarefa já existe na lista`);
+
+    if (this.state.index === -1) {
+      const newTasks = [...tasks];
+      this.setState({
+        tasks: [...newTasks, newTask],
+      });
+      return
+    }
+
+    const index = this.state.index;
+
+    const newTasks = [...tasks]
+    newTasks[index] = newTask
+
+    this.setState({
+      tasks: [...newTasks],
+      index: -1,
+    });
+  };
+
+  handleDelete = (e, index) => {
+    this.setState(this.state.tasks.splice(index, 1));
+  };
+
+  handleEdit = (e, index) => {
+    const oldTask = this.state.tasks[index];
+    this.setState({ newTask: oldTask });
+    this.setState({ index: index });
+  };
+
+  render() {
+    const { tasks, newTask } = this.state;
+    return (
+      <div>
+        <div className="main">
+          <h1>Lista de tarefas</h1>
+          <form onSubmit={this.handleSubmit} action="#" className="form">
+            <input onChange={this.handleChange} type="text" value={newTask} />
+            <button type="submit">
+              <FaPlus />
+            </button>
+          </form>
+          <ul className="tasks">
+            {tasks.map((task, index) => (
+              <li>
+                {task}
+                <span>
+                  <FaEdit
+                    className="edit"
+                    onClick={(e) => this.handleEdit(e, index)}
+                  />
+                  <FaWindowClose
+                    className="delete"
+                    onClick={(e) => this.handleDelete(e, index)}
+                  />
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
